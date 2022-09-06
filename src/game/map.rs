6666810bee23d8d93e166f66208cab5795e71197
MonforteAdrian <adryanmonforte94@gmail.com>
2022-09-06 16:43:32 +0200

    GameState and first sprite

--- src/game/map.rs
+++ src/game/map.rs
@@ -8,14 +8,12 @@ pub struct MapPlugin;
 
 impl Plugin for MapPlugin {
     fn build(&self, app: &mut App) {
-        info!("Hola");
         app
             .add_system_set(SystemSet::on_enter(GameState::MapCreation).with_system(create_map));
     }
 }
 
 fn create_map(mut commands: Commands, asset_server: Res<AssetServer>) {
-        info!("Hola");
     commands.spawn_bundle(SpriteBundle {
         texture: asset_server.load("sprites/cube_green.png"),
         ..default()
